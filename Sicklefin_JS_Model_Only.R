521801359d1d3945ee0738a353dfc95bcd080806
angelahsiung <solitaire.hsiung@gmail.com>
2019-06-03 15:35:39 -0400

    Ran the SRH model multiple times with various number of iterations
    Also was experimenting with autojags and trying different values for function parameters and saved outputs

--- Sicklefin_JS_Model_Only.R
+++ Sicklefin_JS_Model_Only.R
@@ -5,13 +5,14 @@ basedirectory <- "C:/Users/solit/Documents/GitHub/Sicklefin-Redhorse-Analysis"
 setwd(basedirectory)
 
 # Prepare packages
-list.of.packages <- c("stringr", "reshape2", "RMark","lubridate", "tidyverse", "rjags", "coda")
+list.of.packages <- c("stringr", "reshape2", "RMark","lubridate", "tidyverse", "rjags","jagsUI", "coda","parallel")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)){install.packages(new.packages)}
 lapply(list.of.packages, require, character.only = TRUE)
 
-load("SRH_JS_phiSex_pGearEffort_100000.gzip")
-
+# load("SRH_JS_phiSex_pGearEffort_100000.gzip")
+# load("SRH_JS_phiSex_pGearEffort_150000.gzip")
+load("SRH_JS_phiSex_pGearEffort_300000.gzip")
 ###################################################
 ## Operational years (occasion) for each gear type
 ## Seine: 2014-2016 (1-3)
@@ -20,7 +21,7 @@ load("SRH_JS_phiSex_pGearEffort_100000.gzip")
 ###################################################
 
 
-#### Creating matrix of individual and tag type
+#### Creating matrix of individual and tag type ("1" for HDX tags, and "0" for FDX-B tags)
 
 # Read in capture data
 raw.dat<-read.csv("Sicklefin_Capture_Data.csv")
@@ -57,29 +58,32 @@ for(i in 1:nrow(tag.dat)){
 
 
 # prepare tag data for model
-tag.dat<-as.matrix(tag.dat[,1:5])
+tag.dat<-tag.dat[,1:5]
 colnames(tag.dat)<-NULL
 tag.dat[is.na(tag.dat)]<-0
 tag.dat[6,5]<-1 #new HDX tag added to this individual in 2018
 tag.dat<-tag.dat[-278,] #remove fish that was supposedly captured by seine in Valley River
+tag.dat <- apply(tag.dat, 2, as.numeric)
 
 ##### Preparing data for model
 
 # capture history
 ms.cap.hist<-read.csv("Sicklefin Capture History.csv")
 ms.cap.hist<-ms.cap.hist[-278,] #remove fish that was supposedly captured by seine in Valley River
+
+# prepare data for model
 Sex<-ms.cap.hist$Sex
 
 js.CH<-ms.cap.hist
 js.CH$Sex<-NULL
 
-# Convert capture history to data for JS model
+# Convert CJS capture history to data for JS model
 js.CH[is.na(js.CH)]<-1 # if NA, assign 1 for "not seen"
 
 CH.du<-as.matrix(cbind(rep(1, dim(js.CH)[1]), js.CH)) # add extra (dummy) sampling occ in the beginning (Kerry and Schaub 2012)
 colnames(CH.du)<-NULL
 head(CH.du)
-nz<-3500 # augmented individuals
+nz<-4000 # augmented individuals
 
 # augmenting individuals to original capture history
 ms.js.CH.aug<-rbind(CH.du, matrix(1, ncol=dim(CH.du)[2], nrow=nz)) 
@@ -110,7 +114,7 @@ fykeEffort<- c(0, 0, 0, 3, 5, 4) # number of sampling days
 pitEffort <- c(0, 0, 0, 0, 66, 347)
 
 # Bundle data
-dat3<-list(y = ms.js.CH.aug, z=z.known, n.occ = dim(ms.js.CH.aug)[2], Sex=c(Sex, rep(1, nz/2), rep(2, nz/2)), M=dim(ms.js.CH.aug)[1], fykeEffort=fykeEffort, pitEffort=pitEffort, tag.dat.aug=tag.dat.aug)
+dat3<-list(y = ms.js.CH.aug, z=z.known, n.occ = dim(ms.js.CH.aug)[2], Sex=c(Sex, rep(1, nz/2), rep(2, nz/2)), M=dim(ms.js.CH.aug)[1], fykeEffort=fykeEffort, pitEffort=pitEffort, tag.dat.aug=as.matrix(tag.dat.aug))
 
 # Initial values
 
@@ -139,15 +143,16 @@ z.init<-js.multistate.init(CH.du, nz)
 z.init[z.known==2]<-NA
 
 
-inits <- function() {list(z=cbind(rep(NA, nrow(z.init)),z.init[,-1]), phi= runif(2, 0, 1), gamma=runif(5, 0, 1), p0pit=runif(1, 0, 0.01))}
+inits <- function() {list(z=cbind(rep(NA, nrow(z.init)),z.init[,-1]), phi= runif(2, 0, 1), gamma=runif(5, 0, 1),  p0seine=runif(1, 0, 1), p0fyke=runif(1, 0, 1), p0pit=runif(1, 0, 0.01))}
 
 # Parameters monitored
-params <- c("pFyke", "pSeine", "pPit", "phi", "Nsuper", "N", "B", "psi") #"gamma")
+params <- c("pFyke", "pSeine", "pPit", "phi", "gamma", "Nsuper", "N", "B", "psi") 
+codaOnly<-c("Nsuper", "N", "B", "psi")
 
 # MCMC settings
-ni <- 50000
-nt <- 1
-nb <- 100
+ni <- 100000
+nt <- 10
+nb <- 1000
 nc <- 3
 
 
@@ -155,17 +160,88 @@ nc <- 3
 ## 3000 augmented individuals, 100000 iterations
 sr.ms.js.jm1 <- jags.model(data=dat3, inits = inits,
                          file = "ms_js_phiSex_pGearEffort.jags",
-                         n.chains = nc, n.adapt = nb, quiet = F)
+                         n.chains = nc, n.adapt = 100, quiet = F)
 
-sr.ms.js.jc1 <- coda.samples(sr.ms.js.jm1, params, n.iter=ni)
+sr.ms.js.jc1 <- coda.samples(sr.ms.js.jm1, params, n.iter=300)
 
 ## 3500 augmented individuals, 50000 iterations
 sr.ms.js.jm2 <- jags.model(data=dat3, inits = inits,
                            file = "ms_js_phiSex_pGearEffort.jags",
                            n.chains = nc, n.adapt = nb, quiet = F)
+
 sr.ms.js.jc2 <- coda.samples(sr.ms.js.jm2, params, n.iter=ni)
 
+## 4000 augmented individuals, 50000 iterations
+# sr.ms.js.jm3<-jags.model(data=dat3, inits = inits,
+#                          file = "ms_js_phiSex_pGearEffort.jags",
+#                          n.chains = nc, n.adapt = nb, quiet = F)
+# sr.ms.js.jc3 <- coda.samples(sr.ms.js.jm3, params, n.iter=ni)
 
+
+#############################################
+######## Running model using jagsUI #########
+#############################################
+
+## 4000 augmented individuals, 30,000 iterations
+
+sr.ms.js.jm3 <- autojags(dat3, inits, params,
+                           model.file = "ms_js_phiSex_pGearEffort.jags",
+                           n.chains = nc, n.adapt = NULL, iter.increment=1500, 
+                           n.burnin=nb,n.thin=nt,
+                           parallel=TRUE,n.cores=4,Rhat.limit=1, max.iter=30000, verbose=TRUE)
+sr.ms.js.jm3
+plot(sr.ms.js.jm3, ask=TRUE)
+
+## autojags with 60000 max iterations
+sr.ms.js.jm4 <- autojags(dat3, inits, params,
+                         model.file = "ms_js_phiSex_pGearEffort.jags",
+                         n.chains = nc, n.adapt = NULL, iter.increment=1500, 
+                         n.burnin=nb,n.thin=nt,
+                         parallel=TRUE,n.cores=4,Rhat.limit=1, max.iter=60000,verbose=TRUE)
+
+## updating sr.ms.js.jm4 with 40000 more iterations
+
+sr.ms.js.jm5<-update(sr.ms.js.jm4, n.iter=40000)
+plot(sr.ms.js.jm5)
+
+## autojags with 150000 max iterations
+sr.ms.js.jm6 <- autojags(dat3, inits, params,
+                         model.file = "ms_js_phiSex_pGearEffort.jags",
+                         n.chains = nc, n.adapt = NULL, iter.increment=5000,
+                         n.burnin=nb,n.thin=nt,parallel=TRUE,n.cores=4,Rhat.limit=1,
+                         max.iter=150000,verbose=TRUE)
+
+## update sr.ms.js.jm6 50000 iterations
+sr.ms.js.jm7 <- update(sr.ms.js.jm6, n.iter=50000, verbose=TRUE)
+plot(sr.ms.js.jm7)
+
+## autojags 300,000 iterations, 10 thin
+
+sr.ms.js.jm8 <- autojags(dat3, inits, params,
+                         model.file = "ms_js_phiSex_pGearEffort.jags",
+                         n.chains = nc, n.adapt = NULL, iter.increment=10000,
+                         n.burnin=nb, n.thin=nt,parallel=TRUE,n.cores=4,Rhat.limit=1.1,
+                         codaOnly=codaOnly, max.iter=300000,verbose=TRUE)
+
+## autojags 50,000 iterations, 2,000 adapt, 10 thin
+sr.ms.js.jm9 <- autojags(dat3, inits, params,
+                         model.file = "ms_js_phiSex_pGearEffort.jags",
+                         n.chains = nc, n.adapt = 2000, iter.increment=5000,
+                         n.burnin=nb, n.thin=nt,parallel=TRUE,n.cores=3,Rhat.limit=1.1,
+                         codaOnly=codaOnly, max.iter=50000,verbose=TRUE)
+
+#save(sr.ms.js.jc1, file="SRH_JS_phiSex_pGearEffort.gzip")
+#save(sr.ms.js.jc1, file="SRH_JS_phiSex_pGearEffort_100000.gzip")
+#save(sr.ms.js.jc2, file="SRH_SJ_phiSex_pGearEffort_50000.gzip")
+#save(sr.ms.js.jm3, file="SRH_JS_phiSex_pGearEffort_150000.gzip")
+# save(sr.ms.js.jm4, file="SRH_JS_phiSex_pGearEffort_60000.gzip")
+# save(sr.ms.js.jm5, file="SRH_JS_phiSex_pGearEffort_100000_(3).gzip")
+# save(sr.ms.js.jm6, file="SRH_JS_phiSex_pGearEffort_150000_(2).gzip")
+# save(sr.ms.js.jm7, file="SRH_JS_phiSex_pGearEffort_200000.gzip")
+# save(sr.ms.js.jm8, file="SRH_JS_phiSex_pGearEffort_300000.gzip")
+
+
+##rjags output
 out.jc1<-summary(sr.ms.js.jc1) # model output
 out.jc2<-summary(sr.ms.js.jc2)
 
@@ -173,18 +249,13 @@ jc1.stats<-out.jc1$statistics # parameter estimates
 jc1.quants<-out.jc1$quantiles # 95% confidence intervals
 jc2.stats<-out.jc2$statistics # parameter estimates
 jc2.quants<-out.jc2$quantiles
-plot(sr.ms.js.jc1, ask=TRUE)
-
-#save(sr.ms.js.jc1, file="SRH_JS_phiSex_pGearEffort.gzip")
-#save(sr.ms.js.jc1, file="SRH_JS_phiSex_pGearEffort_100000.gzip")
-
 
 # plotting results
-pop.size<-data.frame(cbind(Year=c(2014:2018),pop.est=jc2.stats[6:10, 1], lower=jc2.quants[6:10, 1], upper=jc2.quants[6:10, 5]))
+pop.size<-data.frame(cbind(Year=c(2014:2018),pop.est=sr.ms.js.jm3$mean$N, lower=sr.ms.js.jm3$q2.5$N, upper=sr.ms.js.jm3$q97.5$N))
 pop.size.plot<-ggplot(pop.size, aes(Year, y=pop.est, ymin=lower, ymax=upper))
-pop.size.plot+geom_pointrange(size=1) + labs(y="Population Size")+theme(axis.text=element_text(size=20), axis.title=element_text(size=20, face="bold"))+ylim(0,3100)
+pop.size.plot+geom_pointrange(size=1) + labs(y="Population Size")+theme(axis.text=element_text(size=20), axis.title=element_text(size=20, face="bold"))
 
-recruits<-data.frame(cbind(Year=c(2015:2018),pop.est=jc1.stats[2:5, 1], lower=jc1.quants[2:5, 1], upper=jc1.quants[2:5, 5]))
-recruits.plot<-ggplot(recruits, aes(Year, y=pop.est, ymin=lower, ymax=upper))
-recruits.plot+geom_pointrange(size=1) + labs(y="Number of Recruits")+theme(axis.text=element_text(size=20), axis.title=element_text(size=20, face="bold"))+ylim(0,1000)
+recruits<-data.frame(cbind(Year=c(2015:2018),rec.est=sr.ms.js.jm3$mean$B[2:5], lower=sr.ms.js.jm3$q2.5$B[2:5], upper=sr.ms.js.jm3$q97.5$B[2:5]))
+recruits.plot<-ggplot(recruits, aes(Year, y=rec.est, ymin=lower, ymax=upper))
+recruits.plot+geom_pointrange(size=1) + labs(y="Number of Recruits")+theme(axis.text=element_text(size=20), axis.title=element_text(size=20, face="bold"))
 
